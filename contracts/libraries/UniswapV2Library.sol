92169c3314cf1c607a7ef3f9d6416df95f783721
solidity12 <volibear1001@gmail.com>
2024-07-24 19:34:24 +0900

    feat: first init

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'14d3cbd69c93ad18883dbc035688acb661abe3f1f3f157750307ff1aa2416851' // init code hash
             ))));
     }
 
